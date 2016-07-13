53045c693fe7f1dfd0b31964f3595fd0db907fe4
cicerocasj <cicerocasj@gmail.com>
2016-07-13 11:35:35 -0300

    gera dump estrutura e dados separado

---
+++ gerar_bkp_postgres.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#dump da estrutura
+pg_dump -h host_base -U nome_usuario -d nome_base -f estrutura.sql -s -c -E utf-8 -F p -O -v -x
+
+#dump dos dados
+pg_dump -h host_base -U nome_usuario -d nome_base -f dados.sql -a -b -E utf-8 -F p -O -v -x
